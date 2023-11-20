5552b997839e0ee7c8753cb92106db9a3080f06f
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-11-20 18:55:32 +0000

    retrieve five-day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,20 @@
 var APIKey = "655eb0423f2259c97a977110605cab13";
 var cityName = "London";
 var todaysDate = dayjs().format('DD/MM/YY');
-var geoCodeTest = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + APIKey;
 
+// Declare variables to hold the lat and lon once retrieved via the geoCode API
 var convertLatitude = "";
 var convertLongitude = "";
 
-var todaysWeatherBox = document.getElementById('today');
+// function to convert Kelvin temperature to Celsius
+function kelvinToCelsius (kelvinTemp) {
+  return (kelvinTemp - 273.15).toFixed(1);
+}
+
+console.log(kelvinToCelsius(283.66));
+
+// Query URL for retrieving latitude and longitude
+var geoCodeTest = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + APIKey;
 
 // test using the geocode API to get the long/lat of a searched city name
 function getLatLon() { 
@@ -21,19 +29,18 @@ fetch(geoCodeTest)
     convertLongitude = data[0].lon;
     displayCoords();
     getForecast();
+    fiveDayForecast();
   });
 }
 
+// test to see the coordinates
 function displayCoords() {
 console.log(convertLatitude);
 console.log(convertLongitude);
 }
 
-function kelvinToCelsius (kelvinTemp) {
-  return (kelvinTemp - 273.15).toFixed(1);
-}
-
-console.log(kelvinToCelsius(283.66));
+// Variable to select the div which will hold today's forecast
+var todaysWeatherBox = document.getElementById('today');
 
 function getForecast() {
 // test using the weather API to get the forecast, using the derived long/lat above
@@ -87,3 +94,19 @@ getLatLon();
 
 // make searched city = what was in the search box
 // convert temp to celsius
+
+
+
+function fiveDayForecast() {
+  
+  var fiveDayTest = "https://api.openweathermap.org/data/2.5/forecast?lat=" + convertLatitude + "&lon=" + convertLongitude + "&appid=" + APIKey;
+
+  fetch(fiveDayTest)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (data) {
+    console.log(data);
+  });
+}
