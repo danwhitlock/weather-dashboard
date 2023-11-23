f5a7c4b7d3fbdd15034eeec9a7581078b5f9e206
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-11-23 17:45:07 +0000

    hook up the search button to the forecast functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,23 @@
 var APIKey = "655eb0423f2259c97a977110605cab13";
-var cityName = "London";
+var cityName = "";
+var geoCode = "";
+var queryURLTest = "";
 var todaysDate = dayjs().format('DD/MM/YY');
+
+// Variable to select the div which will hold today's forecast
+var todaysWeatherBox = document.getElementById('today');
+
+// get the searched value and generate the results
+var searchButton = document.getElementById('search-button');
+var searchedValue = document.getElementById('search-input');
+
+searchButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  cityName = searchedValue.value;
+  console.log(searchedValue.value);
+  console.log(cityName);
+  getLatLon();
+}); 
 
 // Declare variables to hold the lat and lon once retrieved via the geoCode API
 var convertLatitude = "";
@@ -11,14 +28,13 @@ function kelvinToCelsius (kelvinTemp) {
   return (kelvinTemp - 273.15).toFixed(1);
 }
 
-console.log(kelvinToCelsius(283.66));
-
-// Query URL for retrieving latitude and longitude
-var geoCodeTest = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + APIKey;
-
-// test using the geocode API to get the long/lat of a searched city name
+// Use the geocode API to get the long/lat of a searched city name
 function getLatLon() { 
-fetch(geoCodeTest)
+
+  // Query URL for retrieving latitude and longitude
+geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + APIKey;
+
+fetch(geoCode)
   .then(function (response) {
     return response.json();
   })
@@ -27,24 +43,24 @@ fetch(geoCodeTest)
     console.log(data);
     convertLatitude = data[0].lat;
     convertLongitude = data[0].lon;
-    displayCoords();
+    // displayCoords();
     getForecast();
     fiveDayForecast();
   });
 }
 
 // test to see the coordinates
-function displayCoords() {
-console.log(convertLatitude);
-console.log(convertLongitude);
-}
+// function displayCoords() {
+// console.log(convertLatitude);
+// console.log(convertLongitude);
+// }
 
-// Variable to select the div which will hold today's forecast
-var todaysWeatherBox = document.getElementById('today');
 
 function getForecast() {
 // test using the weather API to get the forecast, using the derived long/lat above
-var queryURLTest = "https://api.openweathermap.org/data/2.5/weather?lat=" + convertLatitude + "&lon=" + convertLongitude + "&appid=" + APIKey;
+  queryURLTest = "https://api.openweathermap.org/data/2.5/weather?lat=" + convertLatitude + "&lon=" + convertLongitude + "&appid=" + APIKey;
+
+  todaysWeatherBox.innerHTML = "";
 
   fetch(queryURLTest)
   .then(function (response) {
@@ -84,18 +100,13 @@ var queryURLTest = "https://api.openweathermap.org/data/2.5/weather?lat=" + conv
     // style that button
     // make button repeat the search for that city
     // save button in local storage
+    // pull existing buttons from local storage on load
 
     // create header for 5 day forecast
 
   });
 
 }
-getLatLon();
-
-// make searched city = what was in the search box
-// convert temp to celsius
-
-
 
 function fiveDayForecast() {
   
@@ -110,3 +121,8 @@ function fiveDayForecast() {
     console.log(data);
   });
 }
+
+// loop through the objects for records matching 12:00?
+// create a new array of only those objects?
+// loop through those to populate five-day forecast?
+
