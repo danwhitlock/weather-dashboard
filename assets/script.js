76ee88c19de75d01108049d126767d798a02f27d
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-11-25 10:54:26 +0000

    Consolidate into single forecast API call

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var APIKey = "655eb0423f2259c97a977110605cab13";
 var cityName = "";
 var geoCode = "";
-var queryURLTest = "";
+var forecastQueryURL = "";
 var todaysDate = dayjs().format('DD/MM/YY');
 
 // Variable to select the div which will hold today's forecast
@@ -26,7 +26,16 @@ searchButton.addEventListener('click', function(event) {
   savedSearch.textContent = searchedValue.value;
   searchHistoryBox.appendChild(savedSearch);
   getLatLon();
-}); 
+});
+
+// Listeners for previous searches - NOT WORKING!!
+
+searchHistoryBox.addEventListener('click', function(event) {
+  event.preventDefault();
+  cityName = this.value;
+  console.log(cityName);
+  getLatLon();
+});
 
 // Declare variables to hold the lat and lon once retrieved via the geoCode API
 var convertLatitude = "";
@@ -40,38 +49,29 @@ function kelvinToCelsius (kelvinTemp) {
 // Use the geocode API to get the long/lat of a searched city name
 function getLatLon() { 
 
-  // Query URL for retrieving latitude and longitude
-geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + APIKey;
+  geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + APIKey;
 
-fetch(geoCode)
-  .then(function (response) {
-    return response.json();
-  })
+  fetch(geoCode)
+    .then(function (response) {
+      return response.json();
+    })
 
-  .then(function (data) {
-    console.log(data);
-    convertLatitude = data[0].lat;
-    convertLongitude = data[0].lon;
-    // displayCoords();
-    getForecast();
-    fiveDayForecast();
-  });
+    .then(function (data) {
+      console.log(data);
+      convertLatitude = data[0].lat;
+      convertLongitude = data[0].lon;
+      getForecast();
+    });
 }
 
-// test to see the coordinates
-// function displayCoords() {
-// console.log(convertLatitude);
-// console.log(convertLongitude);
-// }
-
+// query the forecast API & display the forecast
 
 function getForecast() {
-// test using the weather API to get the forecast, using the derived long/lat above
-  queryURLTest = "https://api.openweathermap.org/data/2.5/weather?lat=" + convertLatitude + "&lon=" + convertLongitude + "&appid=" + APIKey;
 
+  forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + convertLatitude + "&lon=" + convertLongitude + "&appid=" + APIKey;
   todaysWeatherBox.innerHTML = "";
 
-  fetch(queryURLTest)
+  fetch(forecastQueryURL)
   .then(function (response) {
     return response.json();
   })
@@ -80,7 +80,7 @@ function getForecast() {
     console.log(data);
     // Display City Name
     var searchedCity = document.createElement('h2')
-    searchedCity.textContent = data.name;
+    searchedCity.textContent = data.city.name;
     todaysWeatherBox.appendChild(searchedCity);
     // Display today's date
     var dateContainer = document.createElement('p');
@@ -88,45 +88,26 @@ function getForecast() {
     searchedCity.appendChild(dateContainer);
     // Display Weather Icon
     var weatherImage = document.createElement('img');
-    var weatherImageCode = data.weather[0].icon;
+    var weatherImageCode = data.list[0].weather[0].icon;
     var weatherImageURL = "http://openweathermap.org/img/w/" + weatherImageCode + ".png";
     weatherImage.setAttribute('src', weatherImageURL);
     searchedCity.appendChild(weatherImage);
     // Display temp
     var temp = document.createElement('p');
-    temp.textContent = "Temperature: " + kelvinToCelsius(data.main.temp) + "°C";
+    temp.textContent = "Temperature: " + kelvinToCelsius(data.list[0].main.temp) + "°C";
     searchedCity.appendChild(temp);
     // Display wind
     var wind = document.createElement('p');
-    wind.textContent = "Wind Speed: " + data.wind.speed + "KPH";
+    wind.textContent = "Wind Speed: " + data.list[0].wind.speed + "KPH";
     searchedCity.appendChild(wind);
     // Display humidity 
     var humidity = document.createElement('p');
-    humidity.textContent = "Humidity: " + data.main.humidity + "%";
+    humidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
     searchedCity.appendChild(humidity);
     todaysWeatherBox.setAttribute('style', 'border: 1px solid black');
-    // add button to stored buttons
-    // style that button
-    // make button repeat the search for that city
-    // save button in local storage
-    // pull existing buttons from local storage on load
 
     // create header for 5 day forecast
 
-  });
-}
-
-function fiveDayForecast() {
-  
-  var fiveDayTest = "https://api.openweathermap.org/data/2.5/forecast?lat=" + convertLatitude + "&lon=" + convertLongitude + "&appid=" + APIKey;
-
-  fetch(fiveDayTest)
-  .then(function (response) {
-    return response.json();
-  })
-
-  .then(function (data) {
-    console.log(data);
   });
 }
 
